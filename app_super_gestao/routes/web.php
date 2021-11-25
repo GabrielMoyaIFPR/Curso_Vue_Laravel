9685f64d60e0578aae25a2024cd6722c1f3b05c4
Gabriel Moya <gabrielmoya201415@gmail.com>
2021-11-24 22:10:50 -0300

    Criando rotas de callback

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -18,12 +18,29 @@ Route::get('/sobre-nos', 'SobreNosController@sobrenos')->name('site.sobrenos');
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 Route::get('/login', function(){return 'Login'; })->name('site.login');
 
-Route::prefix('/app')->group(function(){
-    Route::get('/clientes', function(){return 'Clientes'; })->name('app.clientes');
-    Route::get('/fornecedores', function(){return 'Fornecedores'; })->name('app.fornecedores');
-    Route::get('/produtos', function(){return 'Produtos'; })->name('app.produtos');
+// -------------Prefixos------------
+// Route::prefix('/app')->group(function(){
+//     Route::get('/clientes', function(){return 'Clientes'; })->name('app.clientes');
+//     Route::get('/fornecedores', function(){return 'Fornecedores'; })->name('app.fornecedores');
+//     Route::get('/produtos', function(){return 'Produtos'; })->name('app.produtos');
 
+// });
+
+// ---------Redirecionamento de rotas----------
+// Route::get('/rota1', function(){
+//     echo 'Rota 1';
+// })->name('site.rota1');
+
+// Route::get('/rota2', function(){
+//     return redirect()->route('site.rota1');
+// })->name('site.rota2');
+
+// Route::redirect('/rota2', '/rota1');
+
+
+//----------------Rota de contingência(fallback)------------------
+Route::fallback(function(){
+    echo 'Página não encontrada. <a href="'.route('site.index').'">clique aqui</a> para ir para a página inicial.';
 });
-
 
 
