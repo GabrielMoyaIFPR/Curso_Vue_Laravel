56bf843367335eeefad27e3113385e626037f363
Gabriel Moya <gabrielmoya201415@gmail.com>
2021-11-29 22:13:59 -0300

    Criando laços de repetições e suas variações com o Blade

--- app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -11,18 +11,30 @@ class FornecedorController extends Controller
             0=> [
                 'nome'=>'Fornecedor 1',
                 'status'=> 'N',
-                'cnpj'=> '0000000000'
+                'cnpj'=> '0000000000',
+                'ddd'=> '11',
+                'telefone'=> '0000-00000'
         
             ],
             1=> [
                 'nome'=>'Fornecedor 2',
                 'status'=> 'S', 
-                'cnpj'=> ''   
+                'cnpj'=> '',
+                'ddd'=> '85',
+                'telefone'=> '0000-00000'   
+            ],
+            2=> [
+                'nome'=>'Fornecedor 3',
+                'status'=> 'S', 
+                'cnpj'=> '',
+                'ddd'=> '32',
+                'telefone'=> '0000-00000'   
             ]
         ];
+        
 
-        $msg = isset($fornecedores[1]['cnpj']) ? 'CNPJ informado' : 'CNPJ informado';
-        echo $msg;
+        // $msg = isset($fornecedores[1]['cnpj']) ? 'CNPJ informado' : 'CNPJ informado';
+        // echo $msg;
         
         return view('app.fornecedor.index', compact('fornecedores'));
     }
--- app_super_gestao/resources/views/app/Fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/Fornecedor/index.blade.php
@@ -54,13 +54,101 @@
     @endisset
 @endisset    --}}
 
+{{-- SWITCH --}}
 
-@isset($fornecedores)
+{{-- @isset($fornecedores)
     Fornecedor: {{ $fornecedores[1] ['nome'] }}
     <br>
     Status: {{ $fornecedores[1] ['status'] }}
     <br>
-    @isset($fornecedores[1]['cnpj'])
-        CNPJ: {{ $fornecedores[1] ['cnpj'] }}
-    @endisset
+    CNPJ: {{ $fornecedores[1] ['cnpj'] ?? 'Dado não preenchido' }} {{--- Definindo Default em caso de nulo ou vazio ---}}
+    {{-- <br>
+    Telefone: {{ $fornecedores[1] ['ddd'] ?? ''}} {{ $fornecedores[1]['telefone' ?? '']}}
+    @switch($fornecedores[1]['ddd'])
+        @case('11')
+            São Paulo - SP
+            @break
+        @case('32')
+            Juiz de Fora - MG
+            @break
+        @case('85')
+            Fortaleza - CE
+            @break
+        @default
+            Estado não identificado
+    @endswitch
+@endisset --}}
+
+{{-- FOR --}}
+
+{{-- @isset($fornecedores)
+    @for($i = 0; isset($fornecedores[$i]); $i++)
+
+        Fornecedor: {{ $fornecedores[$i] ['nome'] }}
+        <br>
+        Status: {{ $fornecedores[$i] ['status'] }}
+        <br>
+        CNPJ: {{ $fornecedores[$i] ['cnpj'] ?? 'Dado não preenchido' }}
+        <br>
+        Telefone: {{ $fornecedores[$i] ['ddd'] ?? ''}} {{ $fornecedores[1]['telefone' ?? '']}}
+        <br>
+        <br>
+    @endfor
+@endisset --}}
+
+{{-- WHILE --}}
+
+{{-- @isset($fornecedores)
+
+    @php $i = 0 @endphp
+    @while(isset($fornecedores[$i]))
+
+        Fornecedor: {{ $fornecedores[$i] ['nome'] }}
+        <br>
+        Status: {{ $fornecedores[$i] ['status'] }}
+        <br>
+        CNPJ: {{ $fornecedores[$i] ['cnpj'] ?? 'Dado não preenchido' }}
+        <br>
+        Telefone: {{ $fornecedores[$i] ['ddd'] ?? ''}} {{ $fornecedores[1]['telefone' ?? '']}}
+        <br>
+        <br>
+        @php $i++ @endphp
+    @endwhile
+@endisset --}}
+
+{{-- FOREACH --}}
+
+{{-- @isset($fornecedores)
+
+   @foreach($fornecedores as $indice => $fornecedor)
+
+        Fornecedor: {{ $fornecedor['nome'] }}
+        <br>
+        Status: {{ $fornecedor['status'] }}
+        <br>
+        CNPJ: {{ $fornecedor['cnpj'] ?? 'Dado não preenchido' }}
+        <br>
+        Telefone: {{ $fornecedor['ddd'] ?? ''}} {{ $fornecedor['telefone' ?? '']}}
+        <hr>
+    @endforeach    
+@endisset --}}
+
+{{-- FORELSE --}}
+
+@isset($fornecedores)
+
+   @forelse($fornecedores as $indice => $fornecedor)
+
+        Fornecedor: {{ $fornecedor['nome'] }}
+        <br>
+        Status: {{ $fornecedor['status'] }}
+        <br>
+        CNPJ: {{ $fornecedor['cnpj'] ?? 'Dado não preenchido' }}
+        <br>
+        Telefone: {{ $fornecedor['ddd'] ?? ''}} {{ $fornecedor['telefone' ?? '']}}
+        <hr>
+        @empty
+            Não existe fornecedores cadastrados!
+        
+    @endforelse    
 @endisset
