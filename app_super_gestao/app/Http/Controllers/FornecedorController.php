e17fb3ef091da65bcbe4b7b82e1ecc6a51a7a8da
Gabriel Moya <gabrielmoya201415@gmail.com>
2022-03-03 18:57:50 -0300

    CRUD Fornecedor e início do CRUD Produto

--- app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -3,10 +3,89 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Fornecedor;
 class FornecedorController extends Controller
 {
+    //Página Inicial
     public function index() {
-        return view('app.fornecedor');
+        return view('app.fornecedor.index');
+    }
+
+    //Listar Fornecedores
+    public function listar(Request $request) {
+
+        //Filtrar fornecedores de acordo com os dados preenchidos nos campos
+        $fornecedores = Fornecedor::where('nome', 'like', '%'.$request->input('nome').'%')
+                                        ->where('site', 'like', '%'.$request->input('site').'%')
+                                        ->where('uf', 'like', '%'.$request->input('uf').'%')
+                                        ->where('email', 'like', '%'.$request->input('email').'%')
+                                        ->paginate(3);
+
+        return view('app.fornecedor.listar', ['fornecedores' => $fornecedores, 'request' => $request->all() ]);
+    }
+
+    // Adicionar novo cadastro
+    public function adicionar(Request $request) {
+
+        $msg = '';
+
+        // Verificar se o token não está preenchido e se o Id não está informado
+        if($request->input('_token') != '' && $request->input('id') == '') {
+
+            //Validação de regras dos campos e coleta de feedbacks
+            $regras = [
+                'nome'=> 'required|min:3|max:40',
+                'site'=> 'required',
+                'uf'=> 'required|min:2|max:2',
+                'email'=> 'email'
+            ];
+
+            $feedback = [
+                'required' => 'O campo :attribute deve ser preenchido!',
+                'nome.min' => 'O nome deve ter no mínimo 3 caracteres',
+                'nome.max' => 'O nome deve ter no máximo 3 caracteres',
+                'uf.min' => 'UF deve ter no mínimo 2 caracteres',
+                'uf.max' => 'UF nome deve ter no máximo 2 caracteres',
+                'email.email' => 'O e-mail deve ser válido!'
+            ];
+
+            $request->validate($regras, $feedback);
+
+            $fornecedor = new Fornecedor();
+            $fornecedor->create($request->all());
+
+            //Mensagem de sucesso
+            $msg = 'Cadastro realizado com sucesso!';
+        }
+
+        //Edição
+        if($request->input('_token') != '' && $request->input('id') != '') {
+            $fornecedor = Fornecedor::find($request->input('id'));
+            $update = $fornecedor->update($request->all());
+            
+            if($update) {
+                $msg = 'Atualização realizada com Sucesso';
+            }
+            else{
+                $msg = 'Erro ao tentar atualizar';
+            }
+
+            return redirect()->route('app.fornecedor.editar', ['id' => $request->input('id'), 'msg' => $msg]);
+        }
+
+        return view('app.fornecedor.adicionar', ['msg' => $msg]);
+    }
+
+    // Editar um cadastro
+    public function editar($id, $msg = '' ) {
+        $fornecedor = Fornecedor::find($id);
+
+        return view('app.fornecedor.adicionar', ['fornecedor' => $fornecedor, 'msg' => $msg ]);
+    }
+
+    //Excluir um cadastro 
+    public function excluir($id) {
+        Fornecedor::find($id)->delete();
+        return redirect()->route('app.fornecedor');
     }
 }
--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -12,33 +12,46 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-/*
-Route::get('/', function () {
-    return 'Olá, seja bem vindo ao curso!';
-});
-*/
 
+// Home sem login
 Route::get('/', 'PrincipalController@principal')->name('site.index');
         
-
+// Sobre-Nós
 Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
 
+// Contato
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 Route::post('/contato', 'ContatoController@salvar')->name('site.contato');
 
+// Login
 Route::get('/login/{erro?}', 'LoginController@index')->name('site.login');
 Route::post('/login', 'LoginController@autenticar')->name('site.login');
 
+//Usuário Logado
 Route::middleware('autenticacao:padrao, visitante')->prefix('/app')->group(function() {
+
+    // Layout do Menu
     Route::get('/home', 'HomeController@index')->name('app.home');
     Route::get('/sair', 'LoginController@sair')->name('app.sair');
     Route::get('/cliente', 'ClienteController@index')->name('app.cliente');
-    Route::get('/fornecedore', 'FornecedorController@index')->name('app.fornecedor');
-    Route::get('/produto', 'ProdutoController@index')->name('app.produto');
+
+    // Fornecedores
+    Route::get('/fornecedor', 'FornecedorController@index')->name('app.fornecedor');
+    Route::post('/fornecedor/listar', 'FornecedorController@listar')->name('app.fornecedor.listar');
+    Route::get('/fornecedor/listar', 'FornecedorController@listar')->name('app.fornecedor.listar');
+    Route::get('/fornecedor/adicionar', 'FornecedorController@adicionar')->name('app.fornecedor.adicionar');
+    Route::post('/fornecedor/adicionar', 'FornecedorController@adicionar')->name('app.fornecedor.adicionar');
+    Route::get('/fornecedor/editar/{id}/{msg?}', 'FornecedorController@editar')->name('app.fornecedor.editar');
+    Route::get('/fornecedor/excluir/{id}', 'FornecedorController@excluir')->name('app.fornecedor.excluir');
+
+    // Produtos (Cadastro simplificado do Laravel)
+    Route::resource('produto', 'ProdutoController');
 });
 
+// Teste de rota
 Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('site.teste');
 
+// Em caso de página inexistente
 Route::fallback(function() {
     echo 'A rota acessada não existe. <a href="'.route('site.index').'">clique aqui</a> para ir para página inicial';
 });
