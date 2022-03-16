88cff34e75923c94e50c6335cf03c1e121ea28ff
Gabriel Moya <gabrielmoya201415@gmail.com>
2022-03-16 17:47:20 -0300

    Estudando e validando metodos GET, POST, PUT, PATCH e DELETE

--- app_locadora_carros/app/Http/Controllers/MarcaController.php
+++ app_locadora_carros/app/Http/Controllers/MarcaController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 
 class MarcaController extends Controller
 {
+    public function __construct(Marca $marca)
+    {
+        $this->marca = $marca;
+    }
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +18,9 @@ class MarcaController extends Controller
      */
     public function index()
     {
-        //
+        // $marcas = Marca::all();
+        $marcas = $this->marca->all();
+        return response()->json($marcas, 200);
     }
 
     /**
@@ -35,18 +41,27 @@ class MarcaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // $marca = Marca::create($request->all()); 
+
+        $request->validate($this->marca->rules(), $this->marca->feedbacks());
+
+        $marca = $this->marca->create($request->all());
+        return response()->json($marca, 201);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function show(Marca $marca)
+    public function show($id)
     {
-        //
+        $marca = $this->marca->find($id);
+        if($marca === null){
+            return response()->json(['erro' => 'Recurso pesquisado não existe'], 404);
+        }
+        return response()->json($marca, 200);
     }
 
     /**
@@ -64,12 +79,34 @@ class MarcaController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Marca $marca)
+    public function update(Request $request, $id)
     {
-        //
+        $marca = $this->marca->find($id);
+        if($marca === null){
+            return response()->json(['erro' => 'Impossível realizar a atualização. Recurso Inexistente', 404]);
+        }
+
+        if($request->method() === 'PATCH'){
+
+            $regrasDinamicas = array();
+
+
+            foreach($marca->rules() as $input => $regra){
+
+                if(array_key_exists($input, $request->all())){
+                    $regrasDinamicas[$input] = $regra;
+                };
+            }
+            $request->validate($regrasDinamicas, $marca->feedbacks());
+            
+        }else {
+            $request->validate($marca->rules(), $marca->feedbacks());
+        }
+        $marca->update($request->all());
+        return response()->json($marca, 201);
     }
 
     /**
@@ -78,8 +115,13 @@ class MarcaController extends Controller
      * @param  \App\Models\Marca  $marca
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Marca $marca)
+    public function destroy($id)
     {
-        //
+        $marca = $this->marca->find($id);
+        if($marca === null){
+            return response()->json(['erro' => 'Impossível apagar. Recurso Inexistente', 404]);
+        }
+        $marca->delete();
+        return response()->json(['msg' => 'A marca foi removida com sucesso!!', 200]);
     }
 }
--- app_locadora_carros/app/Models/Marca.php
+++ app_locadora_carros/app/Models/Marca.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Marca extends Model
 {
     use HasFactory;
+    protected $fillable = ['nome', 'imagem'];
+
+    public function rules() {
+        return [
+            'nome' => 'required|unique:marcas,nome,'.$this->id.'|min:3',
+            'imagem' => 'required'
+        ];
+    }
+
+    public function feedbacks() {
+        return [
+            'required' => 'O campo :attribute é obrigatório',
+            'nome.unique' => 'O nome da marca já foi cadastrado',
+            'nome.min' => 'O nome da marca deve ter no mínimo 3 caracteres'
+        ];
+    }
 }
